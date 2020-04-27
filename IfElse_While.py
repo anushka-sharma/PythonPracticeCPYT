54966e82bf13c9f14b8855c3cb26e71d55dd7e38
Anushka Sharma <55789857+anushka-sharma@users.noreply.github.com>
2020-04-27 18:26:34 +0530

    Create IfElse_While.py

---
+++ IfElse_While.py
@@ -0,0 +1,27 @@
+#using turtle functions for example
+import turtle
+anushka_turtle=turtle.Turtle()
+
+def square():
+  anushka_turtle.forward(100)
+  anushka_turtle.right(90)
+  anushka_turtle.forward(100)
+  anushka_turtle.right(90)
+  anushka_turtle.forward(100)
+  anushka_turtle.right(90)
+  anushka_turtle.forward(100)
+  #full square
+
+#IF- ELSE STATEMENTS
+elephant_weight= 3000
+ant_weight=0.1
+
+if(elephant_weight>ant_weight):
+   square()
+else:
+   anushka_turtle.forward(100)
+   
+   
+# WHILE LOOP   
+   
+   

ffdaa415bb16ac6f62c53928bccc90018a0d6f19
Anushka Sharma <55789857+anushka-sharma@users.noreply.github.com>
2020-04-27 16:27:36 +0530

    Create Variables&Functions.py

---
+++ Variables&Functions.py
@@ -0,0 +1,27 @@
+import turtle
+
+a= "anushka"
+print(a)
+
+b= 6
+print(6)
+
+credit_card_Number=789654322
+print(credit_card_Number)
+
+
+anushka_turtle=turtle.Turtle()
+
+def square():
+  anushka_turtle.forward(100)
+  anushka_turtle.right(90)
+  anushka_turtle.forward(100)
+  anushka_turtle.right(90)
+  anushka_turtle.forward(100)
+  anushka_turtle.right(90)
+  anushka_turtle.forward(100)
+  #full square
+  
+square()
+anushka_turtle.forward(100)
+square()
